bc3b0a82018f8718caf89de6e390f3e12344df18
sinajamoly <sinajamoly@gmail.com>
2018-06-02 19:04:59 -0700

    private rooms

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {isRealString} = require('./utils/validation')
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 
@@ -22,9 +23,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log('new User connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'))
+
+    socket.on('join',(params, callback)=>{
+        if(!isRealString(params.name) || !isRealString(params.room)){
+            callback('name and room are required');
+        }
+        socket.join(params.room);
+        socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat app'));
+
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+        callback();
+    })
 
     socket.on('createMessage', (message, callback)=>{
         console.log('createMessage', message);
